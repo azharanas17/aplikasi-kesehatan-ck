d6f1232dfccea18429c494d5b2b1f9f0344659e8
azharanas17 <azharanas173@gmail.com>
2025-02-23 06:28:23 +0700

    init filament admin panel

---
+++ resources/views/filament/logo.blade.php
@@ -0,0 +1,4 @@
+<div class="fi-logo flex text-xl font-bold leading-5 tracking-tight text-gray-950 dark:text-white">
+    <img alt="Aplikasi Kesehatan CK" src="/assets/favicon.ico" style="height: 1.5rem; margin-right: 0.5em;">
+    Aplikasi Kesehatan CK
+</div>
